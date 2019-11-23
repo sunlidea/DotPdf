4cd45016a4c6dce76c723dbc3c4978e3de322456
sun li <sun.li@uneed.com>
2019-11-23 23:29:26 +0800

    pdf editor

---
+++ DotPdf/Models/Pdf.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace RazorPagesPdf.Models
+{
+    public class Pdf
+    {
+        [Display(Name = "Family Name")]
+        public string Fam { get; set; }
+        [Display(Name = "Given names")]
+        public string Giv { get; set; }
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime Dob { get; set; }
+        [Display(Name = "Town/city")]
+        public string BirthTown { get; set; }
+        [Display(Name = "Country")]
+        public string BirthCntry { get; set; }
+        [Display(Name = "Passport number")]
+        public string PassNo { get; set; }
+        [Display(Name = "Country of passport")]
+        public string PassCntry { get; set; }
+        [Display(Name = "Relationship status")]
+        public string Restatus { get; set; }
+        [Display(Name = "Identity number")]
+        public string IdentNo { get; set; }
+        [Display(Name = "Country of issue")]
+        public string IdentCntry { get; set; }
+        [Display(Name = "Your present country of citizenship")]
+        public string Cit { get; set; }
+        [Display(Name = "Street")]
+        public string ResiStr { get; set; }
+        [Display(Name = "Sub Address")]
+        public string ResiSub { get; set; }
+        [Display(Name = "Country")]
+        public string ResiCntry { get; set; }
+        [Display(Name = "Postcode")]
+        public string ResiPc { get; set; }
+        [Display(Name = "Street")]
+        public string CorrespStr { get; set; }
+        [Display(Name = "Sub Address")]
+        public string CorrespSub { get; set; }
+        [Display(Name = "Country")]
+        public string CorrespCntry { get; set; }
+        [Display(Name = "Postcode")]
+        public string CorrespHap { get; set; }
+        [Display(Name = "Country code")]
+        public string OffPhCc { get; set; }
+        [Display(Name = "Area code")]
+        public string OffPhAc { get; set; }
+        [Display(Name = "Number")]
+        public string OffPh { get; set; }
+        [Display(Name = "Country code")]
+        public string AfterPhCc { get; set; }
+        [Display(Name = "Area code")]
+        public string AfterPhAc { get; set; }
+        [Display(Name = "Number")]
+        public string AfterPn { get; set; }
+        [Display(Name = "Country code")]
+        public string FaxCc { get; set; }
+        [Display(Name = "Area code")]
+        public string FaxAc { get; set; }
+        [Display(Name = "Number")]
+        public string FaxPh { get; set; }
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+        [Display(Name = "Client number or file number")]
+        public string FileNo { get; set; }
+    }
+}
+
---
+++ DotPdf/Pages/pdf.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPdf.Models;
+using iText.Forms;
+using iText.Kernel.Pdf;
+using System.Reflection;
+using iText.IO.Source;
+using System.IO;
+
+namespace RazorPagesPdfs.Pages.Pdfs
+{
+    public class FillModel : PageModel
+    {
+        public static readonly String SRC = "Resources/5022.pdf";
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Pdf Pdf { get; set; }
+        public string Gender { get; set; }
+        //relationship status
+        public string[] StatusList = { "Married", "Separated", "Engaged","Divorced","De facto","Widowed",
+            "Never married or been in a de facto relationship" };
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //read src file
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(Path.GetFullPath(SRC)), new PdfWriter(baos));
+
+            //genetate pdf form fields
+            PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, false);
+            PropertyInfo[] properties = Pdf.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.GetValue(Pdf, null) == null)
+                {
+                    continue;
+                }
+
+                //field name
+                string name = property.Name;
+                //field value
+                string value = "";
+                string propertyValue = property.GetValue(Pdf, null).ToString();
+                switch (property.Name)
+                {
+                    case "Dob":
+                        //date of birth
+                        value = DateTime.Parse(propertyValue).ToString("dd/MMM/yyyy");
+                        break;
+                    case "Restatus":
+                        //relationship status
+                        name = RelationshipStatus(propertyValue);
+                        value = "yes";
+                        form.GetField(name).SetCheckType(1);
+                        break;
+                    default:
+                        value = propertyValue;
+                        break;
+
+                }
+                //set field name
+                form.GetField(name).SetValue(value);
+            }
+
+            //flat form
+            form.FlattenFields();
+
+            pdfDoc.Close();
+
+            return File(baos.ToArray(), "application/pdf", "result.pdf");
+        }
+
+        //relationship status 
+        protected static string RelationshipStatus(string description)
+        {
+            return description switch
+            {
+                "Married" => "MaritalMar",
+                "Engaged" => "MaritalEng",
+                "De facto" => "MaritalDef",
+                "Separated" => "MaritalSep",
+                "Divorced" => "MaritalDiv",
+                "Widowed" => "MaritalWid",
+                "Never married or been in a de facto relationship" => "MaritalNevmar",
+                _ => "",
+            };
+        }
+    }
+}
